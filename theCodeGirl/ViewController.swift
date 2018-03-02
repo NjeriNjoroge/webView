9288cfc54052c33e6cc992197e371da1fb84edba
NjeriNjoroge <wanjorogegrace@gmail.com>
2018-03-02 16:21:01 +0300

    Initial Commit

---
+++ theCodeGirl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  theCodeGirl
+//
+//  Created by Grace on 02/03/2018.
+//  Copyright © 2018 Grace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
