e8f06c0c7e46902e59636b0c8032098a373a1408
NjeriNjoroge <wanjorogegrace@gmail.com>
2018-04-05 18:30:57 +0300

    url loads but takes too long

--- theCodeGirl/ViewController.swift
+++ theCodeGirl/ViewController.swift
@@ -11,48 +11,25 @@ import WebKit
 
 
 class ViewController: UIViewController, WKNavigationDelegate {
-    
+
     var webView: WKWebView!
-    var activityIndicator: UIActivityIndicatorView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
-        
-        activityIndicator = UIActivityIndicatorView()
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         let url = URL(string: "https://thecodegal.wordpress.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
     
-    func showActivityIndicator(show: Bool) {
-        if show {
-            activityIndicator.startAnimating()
-        } else {
-            activityIndicator.stopAnimating()
-        }
-    }
-    
-    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-        print(error.localizedDescription)
-    }
-    
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        showActivityIndicator(show: true)
-        print("start to load")
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        showActivityIndicator(show: false)
-        print("finish load")
-    }
+
 
 
 }
